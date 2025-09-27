be3367e109c179150ec2a6f5cd550210dfa34338
朱晶晶 <zhujingjing6@huawei.com>
2025-09-27 15:27:18 +0800

    update copyright

--- tests/depends/ascendcl/src/ascendcl_stub.h
+++ tests/depends/ascendcl/src/ascendcl_stub.h
@@ -1,12 +1,12 @@
 /**
-* @file acl_rt.h
-*
-* Copyright (c) Huawei Technologies Co., Ltd. 2019-2020. All rights reserved.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-*/
+ * This program is free software, you can redistribute it and/or modify.
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
 
 #ifndef INC_EXTERNAL_ACL_ACL_RT_STUB_H_
 #define INC_EXTERNAL_ACL_ACL_RT_STUB_H_
