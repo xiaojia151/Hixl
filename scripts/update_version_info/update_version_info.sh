57a80e0f97e0c22cb65113f8a8e21e5bde953c9a
chenkunjie0506 <chenkunjie1@huawei.com>
2025-12-23 09:17:05 +0800

    fix code warning
    Co-authored-by: chenkunjie0506<chenkunjie1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !210 merge warning-fix into master
    
    fix code warning
    
    Created-by: chenkunjie0506
    Commit-by: chenkunjie0506
    Merged-by: cann-robot
    Description: ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 代码重构
    - [ ] 文档更新
    - [x] 其他，请描述：
    修改脚本解释器
    
    ## 描述
    <!--简要描述本次改动的背景，包括改动的原因、解决的问题等。-->
    修改脚本解释器为/bin/bash
    
    ## 测试项
    <!--描述进行了哪些测试来验证本次改动，或新增了哪些测试用例。-->
    
    ## 测试结果
    <!--描述上述测试项的测试结果，可通过表格、图片等形式展示。-->
    
    ## Checklist
    <!--  [x] 表示选中 -->
    - [x] 该Pull Request的代码风格和项目的代码风格一致
    - [x] 提交的代码已经过充分的验证
    - [x] 涉及的相关文档修改已更新
    - [x] 标题中正确使用了类型标签（例如：feat/bugfix/refactor/docs/test等）
    
    ## 其它（可选）
    <!--可以在这里描述任何和该Pull Request相关的说明。-->
    
    
    
    See merge request: cann/hixl-dev!210

--- scripts/update_version_info/update_version_info.sh
+++ scripts/update_version_info/update_version_info.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # ----------------------------------------------------------------------------
 # Copyright (c) 2025 Huawei Technologies Co., Ltd.
 # This program is free software, you can redistribute it and/or modify it under the terms and conditions of
